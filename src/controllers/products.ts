a5a94b98fdcc06555cd353ac1652155657a588bb
mSharifHub <mosharifemail@gmail.com>
2023-08-13 05:36:54 -0700

    returning searchquery from name, vendors, category that mathches the search query and all if no value

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -2,11 +2,28 @@ import { Request, Response } from "express";
 import "express-async-errors";
 import ProductModel from "../models/products";
 
-
 export const getAllProducts = async (req: Request, res: Response) => {
-    const products = await ProductModel.find({});
+    try {
+        let query: any = {};
+        // To handle Unanmed Query Parameter ${searchQuery}for the front end
+        // user can type products/?value
+        const searchQuery = Object.values(req.query)[0];
 
-    res.status(200).json(products);
+        if (typeof searchQuery === "string" && searchQuery.trim() !== "") {
+            const regex = new RegExp(searchQuery, "i");
+
+            query = {
+                $or: [{ name: { $regex: regex } }, { vendors: { $regex: regex } }, { category: { $regex: regex } }],
+            };
+        }
+
+        const products = await ProductModel.find(query);
+
+        res.status(200).json({ products, nbHits: products.length });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ mesasage: `Error occurred\nError:${err}` });
+    }
 };
 
 export const getProductQuery = async (req: Request, res: Response) => {
@@ -16,13 +33,17 @@ export const getProductQuery = async (req: Request, res: Response) => {
         for (const key in req.query) {
             const value = req.query[key];
 
-            if (typeof value === "string") {
-                query[key] = { $regex: new RegExp(value, "i") };
+            if (typeof value != "string") {
+                return res.status(400).json({ message: "No proper value  was provided" });
             }
+            query[key] = { $regex: new RegExp(value, "i") };
+        }
+
+        if (Object.keys(query).length === 0) {
+            return res.status(200).json({ result: query, nbHits: Object.keys(query).length });
         }
 
         const products = await ProductModel.find(query);
-
         res.status(200).json({ products, nbHits: products.length });
     } catch (err) {
         console.log(err);
