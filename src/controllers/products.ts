2e7a41c577b34892fc41a0fa664dd83a402669e8
mSharifHub <mosharifemail@gmail.com>
2023-08-13 22:28:07 -0700

    added numeric and pagination filters

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -27,21 +27,53 @@ export const getAllProducts = async (req: Request, res: Response) => {
         res.status(500).json({ mesasage: `Error occurred\nError:${err}` });
     }
 };
-
 export const getSortedProducts = async (req: Request, res: Response) => {
     try {
-        const { sort } = req.query;
+        const { sort, fields, numericFilters } = req.query;
 
-        const queryObject = {};
+        const query: Record<string, any> = {};
 
-        let results = ProductModel.find(queryObject);
+        if (numericFilters && typeof numericFilters === "string") {
+            const operatorMap: Record<string, string> = {
+                ">": "$gt",
+                ">=": "$gte",
+                "=": "$eq",
+                "<": "$lt",
+                "<=": "$lte",
+            };
 
-        if (typeof sort === "string") {
+            const options: string[] = ["price", "stock"];
+
+            numericFilters.split(",").forEach((option) => {
+                const [field, operator, value] = option.split(/(>|>=|=|<|<=)/);
+
+                if (options.includes(field)) {
+                    query[field] = { [operatorMap[operator]]: Number(value) };
+                }
+            });
+        }
+
+        let results = ProductModel.find(query);
+
+        if (sort && typeof sort === "string") {
             const sortedList: string = sort.split(",").join(" ");
             results = results.sort(sortedList);
-        } else {
+        }
+
+        if (fields && typeof fields === "string") {
+            const fieldList: string = fields.split(",").join(" ");
+            results = results.select(fieldList);
+        }
+
+        if (!sort && !fields && !numericFilters) {
             results = results.sort("createdAt");
         }
+
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
+        results = results.skip(skip).limit(limit);
 
         const products = await results;
 
@@ -50,6 +82,7 @@ export const getSortedProducts = async (req: Request, res: Response) => {
         res.status(500).json({ message: err });
     }
 };
+
 
 export const getProductQuery = async (req: Request, res: Response) => {
     try {
