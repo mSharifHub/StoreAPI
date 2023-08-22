d1142c84933aa883a693de9b2c1cb8849841517b
mSharifHub <mosharifemail@gmail.com>
2023-08-22 03:17:09 -0700

    removes namespace from global.d.ts since is redundant

--- global.d.ts
+++ global.d.ts
@@ -1,13 +1,11 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            PORT: string;
-            IP: string;
-            MONGO_URL: string;
-            JWT_SECRET: string;
-            JWT_LIFETIME: string;
-            NODE_ENV: "development" | "production";
-        }
+    interface ProcessEnv {
+        PORT: string;
+        IP: string;
+        MONGO_URL: string;
+        JWT_SECRET: string;
+        JWT_LIFETIME: string;
+        NODE_ENV: "development" | "production";
     }
 }
 
