68d731d0f2c8fbc4aa2d3d6e4fe7226e52bb8e22
mSharifHub <mosharifemail@gmail.com>
2023-08-26 23:53:28 -0700

    post man API added as json format

--- src/routers/admin.ts
+++ src/routers/admin.ts
@@ -2,8 +2,8 @@ import express from "express";
 import { deleteUser, getAllUsers, singleUser, updateUserEmail, updateUserPassword, updateUsername } from "../controllers/admin";
 const router = express.Router();
 
-router.route("/").get(getAllUsers);
-router.route("/:id").get(singleUser);
+router.route("/get-all-users").get(getAllUsers);
+router.route("/find-user/:id").get(singleUser);
 router.route("/update-username").patch(updateUsername);
 router.route("/update-email").patch(updateUserEmail);
 router.route("/update-password").patch(updateUserPassword);
